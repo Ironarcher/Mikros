2a6c3c6e8b8ac1cd4da70edf44aca08e1aa83ece
Arpad Kovesdy <arpad.kovesdy@gmail.com>
2015-04-24 11:15:26 -0500

    Update server.py

--- mikros/server.py
+++ mikros/server.py
@@ -13,11 +13,42 @@ class Source:
 	youtube = 5
 
 def receive(conn):
-	while True:
-		conn.send("Test from server\n")
-		data=conn.recv(1024)
-		print(data)
+	packet = conn.recv(1024)
+	if packet[0] == "0":
+		#Type: Ping server for latency test. Return immediately
+		reponse = "ping"
+	elif packet[0] = "1":
+		#Type: Use credentials to get followed categories
+	elif packet[0] = "2":
+		#Type: Use credentials to get best articles from selected category
+	elif packet[0] = "3":
+		#Type: Use credentials to get list of sources from selected category
+	elif packet[0] = "4":
+		#Type: Use credentials to rate a selected article based on ID
+	elif packet[0] = "5":
+		#Type: Create a new account by listing e-mail, username, and password
+	elif packet[0] = "6":
+		#Type: LOGIN - Check in account credentials are valid and list login
+	elif packet[0] = "7":
+		#Type: Use credentials to modify password (Require SSL maybe)
+	elif packet[0] = "8":
+		#Type: Use credentials to list an article as viewed
+	else:
+		response = ""
+	conn.send(response)
+	data=conn.recv(1024)
+	print(data)
 
+def getCredentials(packet):
+	data = packet.split()
+	username = data[1]
+	password = data[2]
+	with con:
+		cur = con.cursor()
+		cur.execute("SELECT * FROM users WHERE username =  %s WHERE password = %s", 
+        (username, password))
+        	row = cur.fetchone()
+	
 def getArticles(limit):
 	with con:
 		cur = con.cursor()
